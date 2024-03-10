a60ad3224d24171d9aa4c527c8119b7eac65c76d
meetk1803 <meetkakadiya1803@gmail.com>
2024-03-10 22:18:29 +0530

    10 march

---
+++ app/src/main/java/com/example/tathastu/User_Package/user_Event/user_Event_inDetails.java
@@ -0,0 +1,16 @@
+package com.example.tathastu.User_Package.user_Event;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tathastu.R;
+
+public class user_Event_inDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_event_in_details);
+    }
+}
